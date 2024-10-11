bf68367384eba3982d16ad85e8299e9e699f9c68
Mateus Ferrão da Costa <mateusferraocosta2000@gmail.com>
2024-10-11 17:16:57 -0300

    import json

--- CadastroPaciente/cadastro.js
+++ CadastroPaciente/cadastro.js
@@ -1,7 +1,1 @@
-fetch("usuarios.json").then((resposta) => {
-    resposta.json().then((usuarios) => {
-        console.log(usuarios[0]);
-        let obj = JSON.parse(usuarios);
-        console.log(obj);
-    })
-})
+import data from './usuarios.json';
